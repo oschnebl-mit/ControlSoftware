192da15b8d6dbea6cbbbe530950da92e878b0674
oschnebl <oschnebl@mit.edu>
2025-02-06 13:52:23 -0500

    Rewriting 'build-up' files as threads

---
+++ PressureGauge.py
@@ -0,0 +1,56 @@
+import re, pyvisa
+from pyqtgraph.Qt import QtCore
+
+class PressureGaugeThread(QtCore.QThread):
+    '''
+    Thread that periodically reads from pressure gauge via serial communication
+    '''
+    newData = QtCore.pyqtSignal(object)
+
+    def __init__(self,pyvisaConnection,deviceAddress=None,delay=10):
+        super().__init__()
+        self.running = False
+        self.__connection: pyvisa = pyvisaConnection
+        self.__address: str = deviceAddress
+
+        if self.__address == None:
+            newaddress = self.__ask_address()
+            self.__address = newaddress[7:9]
+
+    def run(self,delay):
+        self.running = True
+        while self.running:
+            self.output = self.get_pressure()
+            self.newData.emit(self.output)
+            QtCore.QThread.msleep(delay)
+
+
+    def __ask_address(self):
+        command = f'@254AD?;FF'
+        response = self.__connection.query(command)
+        return response
+
+    def get_pressure(self):
+        '''NOTE: PR1 - PR4 exist, but seems like PR1-PR3 are the same, PR4 is scientific notation'''
+        command = f'@{self.__address}PR1?;FF'
+        response = self.__connection.query(command)
+        if re.search('\\d*ACK',response) is not None:
+            return float(response.split('ACK')[0:3])
+        else:
+            print(f'Failed to receive pressure reading... Received {response}')
+            return -1
+    
+    def set_gauge_params(self,unit='TORR',address='254',baud_rate='9600'):
+        ''' From manuals, seems to be the same for both models. I have assumed these are the only settings of interest
+        Note the ! sets, while ? is for queries'''
+        response = self.__connection.query( f'@{self.__address}U!{unit};FF')
+        if re.search('\\d*ACK',response) is None:
+            print(f'Failed to set pressure unit... Received {response}')
+        response = self.__connection.query( f'@{self.__address}AD!{address};FF')
+        if re.search(f'\\d*ACK',response) is None:
+            print(f'Failed to set address... Received {response}')
+        else:
+            self.__address = address ## assumes it succeded
+        response = self.__connection.query( f'@{self.__address}BR!{baud_rate};FF')
+        if re.search("\\d*ACK",response) is None:
+            print(f'Failed to set baud rate... Received {response}')
